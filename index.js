a0fa043ce352627b9a8d5623a9d79c04863bf1da
srinu8688 <srinu868890@gmail.com>
2024-06-12 16:49:46 +0530

    cors origin was changed

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-const express=require("express")
-const dotEnv=require("dotenv")
-const mongoose=require("mongoose")
-const vendorRoutes=require("./routes/vendorRoutes")
-const firmRoutes=require("./routes/firmRoutes")
-const productRoutes=require("./routes/productRoutes")
-const bodyParser=require("body-parser");
-const path=require("path")
-const app=express()
-const PORT=process.env.PORT || 4000;
-dotEnv.config()
+const express = require("express");
+const dotEnv = require('dotenv');
+const mongoose = require('mongoose');
+const vendorRoutes = require('./routes/vendorRoutes');
+const bodyParser = require('body-parser');
+const firmRoutes = require('./routes/firmRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cors = require('cors');
+const path = require('path')
+
+const app = express()
+
+const PORT = process.env.PORT || 4000;
+
+dotEnv.config();
+app.use(cors({
+    origin:"https://frontend-dashboard-eight.vercel.app/"
+}))
+
 mongoose.connect(process.env.MONGO_URL)
-    .then(()=>console.log("MongoDB Connected Successfully!"))
-    .catch((error)=>console.log(error))
+    .then(() => console.log("MongoDB connected successfully!"))
+    .catch((error) => console.log(error))
 
 app.use(bodyParser.json());
-app.use("/vendor",vendorRoutes);
-app.use("/firm",firmRoutes)
-app.use("/product",productRoutes);
-app.use("/uploads",express.static("uploads"));
+app.use('/vendor', vendorRoutes);
+app.use('/firm', firmRoutes)
+app.use('/product', productRoutes);
+app.use('/uploads', express.static('uploads'));
 
-app.listen(PORT,()=>{
-    console.log(`server started and running at ${PORT}`)
-})
-app.use("/",(req,res)=>{
-    res.send("<h1> Welcome to Swiggy-clone")
+app.listen(PORT, () => {
+    console.log(`server started and running at ${PORT}`);
+});
+
+app.use('/', (req, res) => {
+    res.send("<h1> Welcome to Swiggy Clone");
 })
